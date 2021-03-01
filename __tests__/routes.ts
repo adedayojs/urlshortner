f5fb1faf6b2d2f83a348f83fafc2f7926b6b564a
adedayojs <aadedunye@gidimobile.com>
2021-03-01 23:34:09 +0100

    Updated test to version one testability

--- __tests__/routes.ts
+++ __tests__/routes.ts
@@ -8,44 +8,44 @@ afterAll(() => {
   });
 });
 describe('Server Presence', () => {
-  test('Has a /api endpoint', () => {
+  test('Has a /api/v1 endpoint', () => {
     return request(app)
-      .get('/api')
+      .get('/api/v1')
       .expect('Content-Type', /json/)
       .expect(200, { message: 'all is well' });
   });
 
-  test('Has a /api/urlshortner endpoint 1', async () => {
-    const req = await request(app).get('/api/urlshortner');
+  test('Has a /api/v1/urlshortner endpoint 1', async () => {
+    const req = await request(app).get('/api/v1/urlshortner');
     expect(req.status).not.toBe(404);
   });
-  test('Has a /api/urlshortner endpoint', async () => {
-    const req = await request(app).post('/api/urlshortner');
+  test('Has a /api/v1/urlshortner endpoint', async () => {
+    const req = await request(app).post('/api/v1/urlshortner');
     expect(req.status).not.toBe(404);
   });
 });
 
 describe('Wrong Request', () => {
   test('Returns bad request when no url is provided to be shortned', async () => {
-    const req = await request(app).post('/api/urlshortner');
+    const req = await request(app).post('/api/v1/urlshortner');
     expect(req.status).toBe(400);
   });
 
   test('Returns bad request when correct bad url is provided to be shortned ', async () => {
-    const req = await request(app).post('/api/urlshortner').send({
+    const req = await request(app).post('/api/v1/urlshortner').send({
       url: 'hdslkdslnknl',
     });
     expect(req.status).toBe(400);
   });
 
   test('Returns bad request when no short id is provided to be queried', async () => {
-    const req = await request(app).get('/api/urlshortner');
+    const req = await request(app).get('/api/v1/urlshortner');
     expect(req.status).toBe(400);
   });
 
   test('Returns Not found when non existent url is requested', async () => {
     const req = await request(app)
-      .get('/api/urlshortner')
+      .get('/api/v1/urlshortner')
       .query({ shortId: 'ddsdfsfs' });
     expect(req.status).toBe(404);
   });
@@ -54,7 +54,7 @@ describe('Wrong Request', () => {
 describe('Correct Request', () => {
   let reply: { shortId: string; url: string };
   test('Returns ok with 201 when correct data is supplied', async () => {
-    const req = await request(app).post('/api/urlshortner').send({
+    const req = await request(app).post('/api/v1/urlshortner').send({
       url: 'https://google.com',
     });
     reply = req.body;
@@ -63,7 +63,7 @@ describe('Correct Request', () => {
   });
 
   test('Returns 200 when same data is supplied', async () => {
-    const req = await request(app).post('/api/urlshortner').send({
+    const req = await request(app).post('/api/v1/urlshortner').send({
       url: 'https://google.com',
     });
     expect(req.status).toBe(200);
@@ -72,7 +72,7 @@ describe('Correct Request', () => {
 
   test('Returns redirect when existent url is requested', async () => {
     const req = await request(app)
-      .get('/api/urlshortner')
+      .get('/api/v1/urlshortner')
       .query({ shortId: reply.shortId });
     expect(req.status).toBe(302);
   });
